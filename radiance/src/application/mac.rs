db5fa7f614d2f4ae5a88f4d3086d5f95fc54c722
Derek Wang <birdinforest@gmail.com>
2020-11-04 02:11:24 +1100

    Revert "Try to fix ownership issue, again."
    This reverts commit 2f666aa6c0b60bd02ff69890f898ee4786a7f33c.

--- radiance/src/application/mac.rs
+++ radiance/src/application/mac.rs
@@ -80,8 +80,4 @@ impl Platform {
     pub fn event_loop(&self) {
         self.events_loop.run(Platform::event_handler);
     }
-
-    pub fn initialize(&self) {
-        Platform::event_loop(self);
-    }
 }
